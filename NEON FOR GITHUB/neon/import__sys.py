a0101b7ef0d1676724bcdd91a4431001566b9091
zodiakraft <zodiakraft@gmail.com>
2022-01-18 22:50:50 +0300

    v.1.0.0
    First version downloaded to GitHub

---
+++ NEON FOR GITHUB/neon/import__sys.py
@@ -0,0 +1,12 @@
+def right():
+    print(12345)
+
+
+class Nasd(object):
+    """docstring for Nasd."""
+
+    def __init__(self):
+        super(Nasd, self).__init__()
+        right()
+
+a = Nasd()
