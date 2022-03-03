068a7b439f9ae9b433ca2feae8be1bb4e6ee07f9
zodiakraft <zodiakraft@gmail.com>
2022-03-03 15:22:00 +0300

    v.1.0.5
    Added facets, segments and coordinates system

--- neon/view.py
+++ neon/view.py
@@ -21,12 +21,20 @@ angle = 0
 
 # cube - 100
 # x y z - пока x и z [0, 2]
-coordinates = [[0, -50, 50, 50, 1, 3, 4], [1, 50, 50, 50, 0, 2, 5], [2, 50, -50, 50, 1, 3, 6], [3, -50, -50, 50, 0, 2, 7],
-               [4, -50, 50, -50, 0, 5, 7], [5, 50, 50, -50, 1, 4, 6], [6, 50, -50, -50, 2, 5, 7], [7, -50, -50, -50, 3, 4, 6]]
+coordinates = [[0, -50, 50, 50], [1, 50, 50, 50], [2, 50, -50, 50], [3, -50, -50, 50],
+               [4, -50, 50, -50], [5, 50, 50, -50], [6, 50, -50, -50], [7, -50, -50, -50]]
 
-def draw_cube(local_angle, local_coordinates):
+segments = [[0, 0, 1], [1, 0, 3], [2, 0, 4], [3, 1, 2], 
+            [4, 1, 5], [5, 2, 3], [6, 2, 6], [7, 3, 7], 
+            [8, 4, 5], [9, 4, 7], [10, 5, 6], [11, 6, 7]]
+
+facets = [[0, 0, 3, 5, 1], [1, 3, 4, 6, 10], [2, 8, 9, 10, 11], [3, 1, 2, 7, 9], [4, 0, 2, 4, 8], [5, 5, 6, 7, 11]]
+
+def draw_cube(local_angle, local_coordinates, local_segments, local_facets):
     
-    polygon_coordinates = sorted(local_coordinates, key=itemgetter(3))
+    index_polygon = lambda lambda_angle: 3 if ((lambda_angle >= 0 and lambda_angle < 45) or (lambda_angle >= 135 and lambda_angle < 225) or (lambda_angle >= 315 and lambda_angle <= 359)) else 1
+    print(index_polygon(local_angle))
+    polygon_coordinates = sorted(local_coordinates, key=itemgetter(index_polygon(local_angle)))
     view_coordinates = [sorted(local_coordinates, key=itemgetter(1))[0][1], sorted(local_coordinates, key=itemgetter(1))[-1][1]]
     print(view_coordinates)
 
@@ -50,19 +58,24 @@ def draw_cube(local_angle, local_coordinates):
     pygame.draw.polygon(screen, color3, ((NX, NY), (NX + K, NY),
                                         (NX + K, NY + K), (NX, NY + K)))
 
-draw_cube(angle, coordinates)
+# draw_cube(angle, coordinates, segments, facets)
 
 while True:
+
+    keys = pygame.key.get_pressed()
+
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
-            sys.exit()
-        elif i.type == pygame.KEYDOWN:
-            if i.key == pygame.K_LEFT:
-                angle -= 1
-                draw_cube(angle, coordinates)
-            elif i.key == pygame.K_RIGHT:
-                angle += 1
-                draw_cube(angle, coordinates)
+            pygame.quit()
+
+    if keys[pygame.K_LEFT]:
+        angle -= 1
+        draw_cube(angle, coordinates, segments, facets)
+        print(angle)
+    elif keys[pygame.K_RIGHT]:
+        angle += 1
+        draw_cube(angle, coordinates, segments, facets)
+        print(angle)
 
     pygame.display.update()
     clock.tick(FPS)
